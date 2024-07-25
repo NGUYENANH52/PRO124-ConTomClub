7e6fac2bcfb2008e576cec958311df8fd5e14d26
Minh Nguyễn <165142704+DuongMinhNguyen@users.noreply.github.com>
2024-07-25 19:15:56 +0700

    chaneges1

---
+++ Assets/Scripts/Scoredata.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+
+    [CreateAssetMenu(fileName = "ScoreData", menuName = "GameData/ScoreData")]
+public class Scoredata : ScriptableObject
+{
+ 
+
+    public int score;
+    
+}
