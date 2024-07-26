bce9548d296c3e9474a0ac94d57444af2e8671a0
LamTran241223 <lamtxph48469@fpt.edu.vn>
2024-07-27 03:30:46 +0700

    laaf nnnn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private ScoreManager scoreManager;
+
+    private void Start()
+    {
+        // Tìm đối tượng ScoreManager trong scene
+        scoreManager = FindObjectOfType<ScoreManager>();
+
+        // Reset điểm khi bắt đầu màn chơi mới
+        if (scoreManager != null)
+        {
+            scoreManager.ResetScore();
+        }
+    }
+
+    // Giả sử có một phương thức để bắt đầu màn chơi mới
+    public void StartNewLevel()
+    {
+        // Reset điểm khi bắt đầu màn chơi mới
+        if (scoreManager != null)
+        {
+            scoreManager.ResetScore();
+        }
+
+        // Các hành động khác để bắt đầu màn chơi mới...
+    }
+}
--- Assets/Scripts/ScoreData.cs
+++ Assets/Scripts/ScoreData.cs
@@ -8,5 +8,13 @@ public class ScoreData : ScriptableObject
 {
     public int score;
     public int highScore;
-
+    public void SaveHighScore()
+    {
+        PlayerPrefs.SetInt("HighScore",highScore);
+        PlayerPrefs.Save();
+    }
+    public void LoadHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
 }
