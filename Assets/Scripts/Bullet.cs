6044d377b73907b027be1311480c905bdbd4af87
NGUYENANH52 <nguyenanh52468@gmail.com>
2024-08-15 00:43:39 +0700

    CHANGE 8.8

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 
 public class bulletScript : MonoBehaviour
 {
--- Assets/Scripts/TableUpdate.cs
+++ Assets/Scripts/TableUpdate.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +10,7 @@ public class TableUpdate : MonoBehaviour
     {
         bool isActive = Update.activeSelf;
         Update.SetActive(!isActive);
+
         if (!isActive)
         {
             Time.timeScale = 0f;
