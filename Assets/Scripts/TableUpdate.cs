218ecb5db4fe5a01d6f493cc8cfeb66f83eaf698
NGUYENANH52 <nguyenanh52468@gmail.com>
2024-08-15 01:00:34 +0700

    CHANGE 8.9

--- Assets/Scripts/TableUpdate.cs
+++ Assets/Scripts/TableUpdate.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 
 public class TableUpdate : MonoBehaviour
 {
-    public GameObject Update;
+    public GameObject Update;  // Màn hình chính cần cập nhật
+
+    // Biến để kiểm tra xem có đang trong màn hình cài đặt hay không
+    private bool isInSettings = false;
+
     public void TogglePauseMenu()
     {
         bool isActive = Update.activeSelf;
         Update.SetActive(!isActive);
-        if (!isActive)
+
+        // Chỉ thay đổi Time.timeScale khi không ở trong màn hình cài đặt
+        if (!isActive && !isInSettings)
         {
             Time.timeScale = 0f;
         }
-        else 
-        { 
+/*        else if (!isInSettings)
+        {
             Time.timeScale = 1f;
-        }
+        }*/
     }
-
 }
