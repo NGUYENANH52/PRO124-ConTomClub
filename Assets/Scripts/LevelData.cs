1c160d7a7762b3265b683f6c617d03797de7215a
LamTran241223 <lamtxph48469@fpt.edu.vn>
2024-08-14 18:22:09 +0700

    lklk;klf

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "GameData/LevelData")]
+public class LevelData : ScriptableObject
+{
+    public int[] experienceToLevelUp; // Mảng chứa EXP yêu cầu cho mỗi cấp độ
+    public float levelMultiplier = 1.5f; // Hệ số tăng EXP yêu cầu sau mỗi lần lên cấp
+
+    // Bạn có thể thêm các thuộc tính khác liên quan đến cấp độ nếu cần
+}
